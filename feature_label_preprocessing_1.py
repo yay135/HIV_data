6c9e2a130d1004106a5dbf9261db9bb0d202c835
jjin49 <43580189+jjin49@users.noreply.github.com>
2019-11-13 15:18:47 -0500

    Add files via upload

---
+++ feature_label_preprocessing_1.py
@@ -0,0 +1,165 @@
+
+# the label will be two number. Each number refers to the number of months between two concecutive visits. eg [2,3]
+# the input csv file should at least contain three columns: 
+
+
+import os
+import math
+import random
+
+import torch
+import pandas as pd
+from datetime import datetime as dt
+import datetime as datetime
+import numpy as np
+import sys
+
+
+
+class DataPreprocessing:
+    def __init__(self, feature_encoder, batch_size, seq_length=50, test_ratio=0.1,validation_ratio=0.1, shuffle=True):
+        self.batch_size = batch_size
+        self.feature_encoder = feature_encoder
+        self.shuffle = shuffle
+        self.validation_ratio = validation_ratio        
+        self.seq_length = seq_length
+
+        self.batch_index_train = 0
+        self.batch_index_val = 0
+        self.batch_index_test = 0
+
+        self.samples = []
+        self.read_samples()  
+        self.train_samples = []
+        self.validation_samples = []
+        self.test_samples = []   
+        self.split_data()     
+
+    def train_steps(self):
+        return math.ceil(len(self.train_samples) / self.batch_size)
+
+    def val_steps(self):
+        return math.ceil(len(self.validation_samples) / self.batch_size)
+
+    def test_steps(self):
+        return math.ceil(len(self.weekly_samples) / self.batch_size)
+
+
+    def read_samples(self):
+
+        r = pd.read_csv("data.csv") 
+        visit_record = r[r.TEST.str.contains("CD4") | r.TEST.str.contains("VL")]  
+
+        visit_id_date = visit_record[['RFA_ID','TESTDATE']]
+        visit_id_date = visit_id_date.drop_duplicates()	
+
+        index = visit_id_date.RFA_ID.drop_duplicates().tolist()
+
+        for i in index:
+            s = visit_id_date[visit_id_date.RFA_ID == i]  
+            s = s.TESTDATE.tolist()
+            if len(s)<3:
+               continue
+            s = [dt.strptime(x,'%m/%d/%Y') for x in s]
+            s.sort()
+            self.samples.append((i,[(s[-2]-s[-3]).days/30,(s[-1]-s[-2]).days/30]))       
+
+        if self.shuffle:
+            random.shuffle(self.samples)
+
+
+    def split_data(self):
+        """Split training and validation data randomly
+        """
+        train_val_count = math.ceil(len(self.samples) * (1 - self.test_ratio))
+        train_count = train_val_count * (1 - self.validation_ratio)
+        self.train_samples = self.samples[:train_count]
+        self.validation_samples = self.samples[train_count:train_val_count]
+        self.test_samples = self.samples[train_val_count:]
+
+
+    def batch_train_data(self):
+        """A batch of training data
+        """
+        data = self.batch(self.batch_index_train, self.train_samples)
+        self.batch_index_train += 1
+        return data
+
+    def batch_val_data(self):
+        """A batch of validation data
+        """
+        data = self.batch(self.batch_index_val, self.validation_samples)
+        self.batch_index_val += 1
+        return data
+
+    def batch_test_data(self):
+        """A batch of test data
+        """
+        data = self.batch(self.batch_index_test, self.test_samples, testing=True)
+        self.batch_index_test += 1
+        return data
+
+    def new_epoch(self):
+        """New epoch. Reset batch index
+        """
+        self.batch_index_train = 0
+        self.batch_index_val = 0
+        self.batch_index_test = 0
+     
+
+   def batch(self, batch_index, sample_set, testing=False):
+        """Get a batch of samples
+        """
+        seq_tensors = []
+
+        label_list = []
+
+        # for testing
+        patient_id_list = []
+
+        def encode_sample(sample):
+            hla_a_allele = sample[0]
+            hla_b_allele = sample[1]
+            pep = sample[2]
+
+            if not testing:
+                label = torch.tensor(sample[1])
+            else:
+                pid = sample[0]
+
+            seq_tensor = self.feature_encoder(sample[0])
+            seq_tensors.append(seq_tensor)
+
+            if not testing:
+                label_list.append(label)
+            else:
+                patient_id_list.append(pid)
+
+        start_i = batch_index * self.batch_size
+        end_i = start_i + self.batch_size
+        for sample in sample_set[start_i: end_i]:
+            encode_sample(sample)
+
+        # in case last batch does not have enough samples, random get from previous samples
+        if len(seq_tensors) < self.batch_size:
+            for i in random.sample(range(start_i), self.batch_size - len(seq_tensors)):
+                encode_sample(sample_set[i])
+
+        if not testing:
+            return (
+                torch.stack(seq_tensors, dim=0),
+                torch.stack(label_list,dim=0)
+            )
+        else:
+            return (
+                torch.stack(seq_tensors, dim=0),
+                patient_id_list
+            )
+
+
+def main():
+    test()
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ lstm1.py
@@ -0,0 +1,156 @@
+
+# to use th model, when we pad the sequences, the zeros need to be all in the left of the sequences.
+# for this version, we only need to predict next periods of next two visits 
+
+import torch
+import torch.nn as nn
+
+
+###################################################################
+
+# Weight initial setup
+
+def weight_initial(model):
+    for m in model.modules():
+        if isinstance(m, nn.Conv1d):
+            nn.init.xavier_uniform_(m.weight.data, gain=1.0)
+            if m.bias is not None:
+               nn.init.constant_(m.bias.data, 0.0)
+        elif isinstance(m, nn.LSTM):
+            for name, param in m.named_parameters():
+                if 'bias' in name:
+                    nn.init.constant_(param, 0.0)
+                elif 'weight' in name:
+                    nn.init.uniform_(param, a=-0.01, b=0.01)
+        elif isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight.data, gain=1.0)
+            nn.init.constant_(m.bias.data, 0.0)
+
+###################################################################
+
+
+###################################################################
+# feature preprocessing
+
+# suppose the shape of raw input is [batch_size, seq_len, input_channels] 
+
+# need to consider if we put time-variant feature with time-invariant feature together to the fc layers or we just
+# put them to separate fc layers and then concat with output.
+
+# mask shape [batch_size, seq_len]
+# the output here will be  [batch_size, seq_len, 40]
+
+class Post_encoder(nn.Module):
+
+    def __init__(self, inv_size, v_size):
+        super(Post_encoder, self).__init__()
+        self.net_inv = nn.Sequential(
+            nn.Linear(inv_size, 100),
+            nn.LeakyReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(100, 50),
+            nn.LeakyReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(50, 20)
+        )
+
+        self.net_v = nn.Sequential(
+            nn.Linear(v_size, 20),
+            nn.LeakyReLU(),
+            nn.Linear(20, 20)
+        )
+
+        self.out_act = nn.tanh()
+
+    def forward(self, raw_inv,raw_v,mask):
+        out_inv = self.net_inv(raw_inv)
+        out_inv = self.out_act(out_inv)
+        out_v = self.net_v(raw_v)
+        out_v = self.out_act(out_v)
+        out = torch.cat([out_v, out_inv], dim = 2)
+        mask =torch.reshape(mask, (mask.size(0),mask.size(1),1))
+        # to mask out the padding positions.
+        out = mask * out
+
+        return out
+
+
+###################################################################
+
+
+###################################################################
+
+# LSTM_model
+
+class LSTM_main(nn.Module):
+    def __init__(self, input_channels, hidden_size, output_size, batch_size, layers,  bias=False):
+        super(LSTM_main, self).__init__()
+        self.lstm = nn.LSTM(
+            input_channels,
+            hidden_size,
+            num_layers=layers,
+            bias=bias,
+            batch_first=True,
+            bidirectional=False,
+            dropout=0.5
+        )
+        self.layers = layers
+        self.num_directions = 1
+        self.batch_size = batch_size
+        self.hidden_size = hidden_size
+        self.hidden_states = None
+
+        self.act = nn.LeakyReLU()
+
+        self.linear = nn.Linear(self.hidden_size, self.output_size)
+
+
+    def init_hidden_state(self):
+        return (
+            torch.zeros(self.layers * self.num_directions,
+                        self.batch_size, self.hidden_size),
+            torch.zeros(self.layers * self.num_directions,
+                        self.batch_size, self.hidden_size)
+        )         
+
+
+    def forward(self, seq_input):
+    	# dimension of seq_input : [batch_size, seq_length, channel_size ]
+
+        self.hidden_states = self.init_hidden_state()
+
+        out, self.hidden_states = self.lstm(seq_input, self.hidden_states)
+
+        # This will ouput the hidden state of the last layer at last timestamp. The output size will be [batch_size, hidden_size]
+        temp = self.hidden_states[0][self.layers-1]
+
+        out = self.act(temp)
+
+        # this will output a vector of [batch_size, 2]
+        out = self.linear(out)
+
+        return out     
+
+###################################################################
+
+###################################################################
+
+# main model
+
+class Model(nn.Module):
+    def __init__(self, inv_size, v_size, input_channels, hidden_size, output_size, batch_size, layers):
+        super(Model, self).__init__()
+
+        self.feature_compress = Post_encoder(inv_size, v_size)
+        self.lstm_predictor = LSTM_main(input_channels, hidden_size, output_size, batch_size,layers)
+
+    def forward( self, raw_inv,raw_v,mask, self, seq_input):
+
+        post_seq = self.feature_compress( raw_inv, raw_v, mask )
+        predict_results = self.lstm_predictor (seq_input)
+
+        return predict_results
+
+
